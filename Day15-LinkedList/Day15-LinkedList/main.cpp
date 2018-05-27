d8381c75dbc8feeadf500f0a2646e703311aabc4
Larry Lai <cgpanda.sg@gmail.com>
2018-05-27 22:56:31 +0800

    Day15-LinkedList

---
+++ Day15-LinkedList/Day15-LinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Day15-LinkedList
+//
+//  Created by Larry Lai on 27/5/18.
+//  Copyright © 2018 Larry Lai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
